4e3010313cff7351b9e3c2e526d810d062c48231
PradyBoi <PradyBoi@outlook.com>
2022-10-14 10:19:15 +0530

    chnges

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,13 +1,12 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
-import { User } from '../entities/entities';
+import { User } from './entities';
 
 // const AppDataSource = new DataSource({
 export const AppDataSource = new DataSource({
   type: 'mongodb',
-  url: 'mongodb+srv://PradyumnKhare:Nopassword@1@cluster0.51ma1qm.mongodb.net/ProjectDB?retryWrites=true&w=majority',
+  url: 'mongodb+srv://PradyumnKhare:Nopassword@1@cluster0.wuhocrp.mongodb.net/typeORMWithMongoDB?retryWrites=true&w=majority',
   useNewUrlParser: true,
   useUnifiedTopology: true,
   entities: [User],
 });
-
